68268ab193fcd030dd84afb08a2d83e699ba45c4
youssef <youssefelghamour@gmail.com>
2024-03-04 13:52:57 -0800

    correct 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,18 +3,18 @@
 exec {'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
-}
+} ->
 
 exec {'install Nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx'
-}
+} ->
 
 file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-}
+} ->
 
 exec {'start nginx':
   command => '/usr/sbin/service nginx start',
