5a9e24c11062d5fd2528d13b4d1538301ad45734
youssef <youssefelghamour@gmail.com>
2024-03-04 13:37:05 -0800

    correct 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,16 +4,16 @@ exec {'update':
   command => '/usr/bin/apt-get update',
 }
 
-package {'nginx':
+-> package {'nginx':
   ensure => 'present',
 }
 
-file_line { 'http_header':
+-> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
-service { 'nginx':
+-> service { 'nginx':
   command => '/usr/sbin/service nginx start',
 }
