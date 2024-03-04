da59dca4dd94761ee3ae1494349812aaeafcda69
youssef <youssefelghamour@gmail.com>
2024-03-04 12:34:39 -0800

    correct 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,14 +9,9 @@ file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
-exec { 'configure_nginx':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/youssefelghamour permanent;/" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-
 exec { 'custom_header':
   environment => ["HOSTNAME=${hostname}"],
-  command  => 'sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
+  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
   provider => shell,
 }
 
