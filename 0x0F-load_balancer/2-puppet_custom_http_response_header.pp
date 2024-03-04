78218d3b227b8c1bbbf2ec57e918e5cbe324bf07
youssef <youssefelghamour@gmail.com>
2024-03-04 12:36:39 -0800

    correct 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,11 +4,6 @@ package { 'nginx':
   ensure => installed,
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-}
-
 exec { 'custom_header':
   environment => ["HOSTNAME=${hostname}"],
   command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
