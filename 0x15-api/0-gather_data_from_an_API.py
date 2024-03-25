3afa55865abc502564fdd7a14bb10e423e3602cb
youssef <youssefelghamour@gmail.com>
2024-03-25 10:23:30 -0700

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url_users = "https://jsonplaceholder.typicode.com/users/"
+    url_todos = "https://jsonplaceholder.typicode.com/todos/"
+
+    user_id = sys.argv[1]
+
+    url_users = url_users + user_id
+    url_todos = url_todos + "/?userId=" + user_id
+
+    users = requests.get(url_users).json()
+    todos = requests.get(url_todos).json()
+
+    print("Employee {} is done with tasks".format(users['name']), end="")
+
+    nb_tasks = 0
+    nb_tasks_completed = 0
+
+    for task in todos:
+        nb_tasks += 1
+        if task['completed'] is True:
+            nb_tasks_completed += 1
+
+    print("({}/{}):".format(nb_tasks_completed, nb_tasks))
+
+    for task in todos:
+        if task['completed'] is True:
+            print("\t {}".format(task['title']))
