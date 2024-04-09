f801a9908ca20b189938aca0e75616c99c102296
youssef <youssefelghamour@gmail.com>
2024-04-09 09:58:11 -0700

    3. Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 """ function that queries the Reddit API """
 import requests
+after = None
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
+def count_words(subreddit, word_list):
+    """Count the titles found with wordlist in subreddit"""
+    my_list = recurse(subreddit)
+    my_dict = {}
+
+    if my_list:
+        for word in word_list:
+            word = word.lower()
+            my_dict[word] = 0
+
+        for title in my_list:
+            title_split = title.split(" ")
+
+            for iter in title_split:
+                for iter_split in word_list:
+                    if iter.lower() == iter_split.lower():
+                        my_dict[iter_split.lower()] = my_dict.get(iter_split.lower(), 0) + 1
+
+        for key, val in sorted(my_dict.items(),  key=lambda x: x[1],
+                               reverse=True):
+            if val != 0:
+                print("{}: {}".format(key, val))
+
+
+def recurse(subreddit, hot_list=[]):
     """ recursively queries the Reddit API, parses titles,
         and counts keyword occurrences
     """
-
-    if counts is None:
-        counts = {}
-
+    global after
     CLIENT_ID = 'zL8evAsF03DzVk_TArVH3g'
     SECRET_KEY = 'FT7BTQs-WLEtZEOlH_2mE2QEQ8CV2g'
 
@@ -36,33 +58,16 @@ def count_words(subreddit, word_list, after=None, counts=None):
     response = requests.get(url, auth=auth, data=data, headers=headers,
                             params=params, allow_redirects=False)
 
-    if response.status_code != 200:
-        return None
+    if response.status_code == 200:
+        prox = response.json().get('data').get('after')
 
-    data = response.json()['data']
-    posts = data['children']
-    after = data['after']
+        if prox is not None:
+            after = prox
+            recurse(subreddit, hot_list)
+        list_titles = response.json().get('data').get('children')
 
-    for post in posts:
-        title = post['data']['title'].lower()
-        for word in word_list:
-            word = word.lower()
-
-            if word in title:
-                # Get the current count for the word or default to 0
-                count = counts.get(word, 0)
-                # Increment the count by 1 and update the dictionary
-                counts[word] = count + 1
-
-    if after:
-        return count_words(subreddit, word_list, after, counts)
+        for title_ in list_titles:
+            hot_list.append(title_.get('data').get('title').lower())
+        return hot_list
     else:
-        # Converts the counts dict into a list of tuples [(word, count), ...]
-        # Sorts the list in descending order by turning the counts (x[1] in
-        # the tuple) to negative numbers during sorting,
-        # which ensures that bigger numbers come first
-        # When words have the same count, they are sorted alphabetically (x[0])
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-
-        for word, count in sorted_counts:
-            print("{}: {}".format(word, count))
+        return (None)
